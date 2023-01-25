11ece274c7aa38e91bd866f6d20c3c4a450c140d
dimitri-hoareau-WEL <dimitri.hoareau@weuplearning.com>
2023-01-25 11:14:28 +0100

    first commit

---
+++ cms/envs/tutor/production.py
@@ -0,0 +1,274 @@
+# -*- coding: utf-8 -*-
+import os
+from cms.envs.production import *
+
+####### Settings common to LMS and CMS
+import json
+import os
+
+from xmodule.modulestore.modulestore_settings import update_module_store_settings
+
+# Mongodb connection parameters: simply modify `mongodb_parameters` to affect all connections to MongoDb.
+mongodb_parameters = {
+    "db": "openedx",
+    "host": "mongodb",
+    "port": 27017,
+    "user": None,
+    "password": None,
+    # Connection/Authentication
+    "ssl": False,
+    "authSource": "admin",
+    "replicaSet": None,
+    
+}
+DOC_STORE_CONFIG = mongodb_parameters
+CONTENTSTORE = {
+    "ENGINE": "xmodule.contentstore.mongo.MongoContentStore",
+    "ADDITIONAL_OPTIONS": {},
+    "DOC_STORE_CONFIG": DOC_STORE_CONFIG
+}
+# Load module store settings from config files
+update_module_store_settings(MODULESTORE, doc_store_settings=DOC_STORE_CONFIG)
+DATA_DIR = "/openedx/data/modulestore"
+
+for store in MODULESTORE["default"]["OPTIONS"]["stores"]:
+   store["OPTIONS"]["fs_root"] = DATA_DIR
+
+# Behave like memcache when it comes to connection errors
+DJANGO_REDIS_IGNORE_EXCEPTIONS = True
+
+# Elasticsearch connection parameters
+ELASTIC_SEARCH_CONFIG = [{
+  
+  "host": "elasticsearch",
+  "port": 9200,
+}]
+
+# Common cache config
+CACHES = {
+    "default": {
+        "KEY_PREFIX": "default",
+        "VERSION": "1",
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://@redis:6379/1",
+    },
+    "general": {
+        "KEY_PREFIX": "general",
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://@redis:6379/1",
+    },
+    "mongo_metadata_inheritance": {
+        "KEY_PREFIX": "mongo_metadata_inheritance",
+        "TIMEOUT": 300,
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://@redis:6379/1",
+    },
+    "configuration": {
+        "KEY_PREFIX": "configuration",
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://@redis:6379/1",
+    },
+    "celery": {
+        "KEY_PREFIX": "celery",
+        "TIMEOUT": 7200,
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://@redis:6379/1",
+    },
+    "course_structure_cache": {
+        "KEY_PREFIX": "course_structure",
+        "TIMEOUT": 7200,
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://@redis:6379/1",
+    },
+}
+
+# The default Django contrib site is the one associated to the LMS domain name. 1 is
+# usually "example.com", so it's the next available integer.
+SITE_ID = 2
+
+# Contact addresses
+CONTACT_MAILING_ADDRESS = "Olive Test Dimitri - http://local.overhang.io"
+DEFAULT_FROM_EMAIL = ENV_TOKENS.get("DEFAULT_FROM_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+DEFAULT_FEEDBACK_EMAIL = ENV_TOKENS.get("DEFAULT_FEEDBACK_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+SERVER_EMAIL = ENV_TOKENS.get("SERVER_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+TECH_SUPPORT_EMAIL = ENV_TOKENS.get("TECH_SUPPORT_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+CONTACT_EMAIL = ENV_TOKENS.get("CONTACT_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+BUGS_EMAIL = ENV_TOKENS.get("BUGS_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+UNIVERSITY_EMAIL = ENV_TOKENS.get("UNIVERSITY_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+PRESS_EMAIL = ENV_TOKENS.get("PRESS_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+PAYMENT_SUPPORT_EMAIL = ENV_TOKENS.get("PAYMENT_SUPPORT_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+BULK_EMAIL_DEFAULT_FROM_EMAIL = ENV_TOKENS.get("BULK_EMAIL_DEFAULT_FROM_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+API_ACCESS_MANAGER_EMAIL = ENV_TOKENS.get("API_ACCESS_MANAGER_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+API_ACCESS_FROM_EMAIL = ENV_TOKENS.get("API_ACCESS_FROM_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+
+# Get rid completely of coursewarehistoryextended, as we do not use the CSMH database
+INSTALLED_APPS.remove("lms.djangoapps.coursewarehistoryextended")
+DATABASE_ROUTERS.remove(
+    "openedx.core.lib.django_courseware_routers.StudentModuleHistoryExtendedRouter"
+)
+
+# Set uploaded media file path
+MEDIA_ROOT = "/openedx/media/"
+
+# Video settings
+VIDEO_IMAGE_SETTINGS["STORAGE_KWARGS"]["location"] = MEDIA_ROOT
+VIDEO_TRANSCRIPTS_SETTINGS["STORAGE_KWARGS"]["location"] = MEDIA_ROOT
+
+GRADES_DOWNLOAD = {
+    "STORAGE_TYPE": "",
+    "STORAGE_KWARGS": {
+        "base_url": "/media/grades/",
+        "location": "/openedx/media/grades",
+    },
+}
+
+ORA2_FILEUPLOAD_BACKEND = "filesystem"
+ORA2_FILEUPLOAD_ROOT = "/openedx/data/ora2"
+ORA2_FILEUPLOAD_CACHE_NAME = "ora2-storage"
+
+# Change syslog-based loggers which don't work inside docker containers
+LOGGING["handlers"]["local"] = {
+    "class": "logging.handlers.WatchedFileHandler",
+    "filename": os.path.join(LOG_DIR, "all.log"),
+    "formatter": "standard",
+}
+LOGGING["handlers"]["tracking"] = {
+    "level": "DEBUG",
+    "class": "logging.handlers.WatchedFileHandler",
+    "filename": os.path.join(LOG_DIR, "tracking.log"),
+    "formatter": "standard",
+}
+LOGGING["loggers"]["tracking"]["handlers"] = ["console", "local", "tracking"]
+# Silence some loggers (note: we must attempt to get rid of these when upgrading from one release to the next)
+
+import warnings
+from django.utils.deprecation import RemovedInDjango40Warning, RemovedInDjango41Warning
+warnings.filterwarnings("ignore", category=RemovedInDjango40Warning)
+warnings.filterwarnings("ignore", category=RemovedInDjango41Warning)
+warnings.filterwarnings("ignore", category=DeprecationWarning, module="lms.djangoapps.course_wiki.plugins.markdownedx.wiki_plugin")
+warnings.filterwarnings("ignore", category=DeprecationWarning, module="wiki.plugins.links.wiki_plugin")
+
+# Email
+EMAIL_USE_SSL = False
+# Forward all emails from edX's Automated Communication Engine (ACE) to django.
+ACE_ENABLED_CHANNELS = ["django_email"]
+ACE_CHANNEL_DEFAULT_EMAIL = "django_email"
+ACE_CHANNEL_TRANSACTIONAL_EMAIL = "django_email"
+EMAIL_FILE_PATH = "/tmp/openedx/emails"
+
+# Language/locales
+LOCALE_PATHS.append("/openedx/locale/contrib/locale")
+LOCALE_PATHS.append("/openedx/locale/user/locale")
+LANGUAGE_COOKIE_NAME = "openedx-language-preference"
+
+# Allow the platform to include itself in an iframe
+X_FRAME_OPTIONS = "SAMEORIGIN"
+
+
+JWT_AUTH["JWT_ISSUER"] = "http://local.overhang.io/oauth2"
+JWT_AUTH["JWT_AUDIENCE"] = "openedx"
+JWT_AUTH["JWT_SECRET_KEY"] = "nxZdmMLVeIqBLDPAuatdQaIr"
+JWT_AUTH["JWT_PRIVATE_SIGNING_JWK"] = json.dumps(
+    {
+        "kid": "openedx",
+        "kty": "RSA",
+        "e": "AQAB",
+        "d": "AQ7cEmNjZpJC7kj4D4ZAuGEpJeW0SwBlqO3XUUIKLlXYWgiAAZMzScd49mbR1XtlkLBSsvfgBOfCg2W5GQTzex50Pvrz7Rd0xYDTahnDaLfcBxsH5uefi0NeFW4HaOM1UXK2y8pct-inDZgsAMk1LeUfvknYdMFlb1QXfaC-cSSnIAANQFMyGVVCZ3BNFF5T7i3qRMYfMEkhVNSAprhlNaOv_vP8VvwD2NHjtdeFg9uWpHuvoU14vTMoxhGdMmwPVv8HJaf3RLW01GF9nBaU4O3Z4Wc7HFYosn2nRTFEA2TtKD-wxslfr4n9Xhix8ceTi8itr71vcuyY97e5pOK-eQ",
+        "n": "nP-wWVXa4Xh3VSV9SpEMJvV8dZPp_z2hUvuy97S11R-BL3FvhjSqlySPSXxedcwN9eNv18JcCF9-cub6DfBya2K_VYNTr0SCZDwKMoe74jVfCbHBXQtF4BHIbpR7zDExpfvkFYmqX3dhU5iYB6hDfS5ad4zkuDwBMWFFld8x-a9sY_ZNCRwe2-Fk6VfTn7o2DBi4owhlPoc03Z2cGW4j56PHXAJJtwJ41VAE2sQaf5rmv_9A_WHe0vtAr43kUwY2Xwn3IP3rJNacSPOtUm2t8z2UlAP7rwueeWo1NLpOlenNg1_J0BVylOnC4_lewef7JpsrbZgjtty8Rf7YNBebdw",
+        "p": "tQ9ssVyewm2MGaHEmbZOhN2DuLShXQIXrrHKeyPjjiCSlXG49XA1iHK_Zu7rIYtsNAvOCznKsKCdAW3aVLjwuRw46e762ZIFqihl0wYHetagiahzSaNus-d9RUwr0V6Xga9C0mhnbdzKSJKPa_1HcSc_qL4UcKD9gVqZ9VYOmpU",
+        "q": "3frJaf6JewmoVR1u_T4yIuFjeN3A5Tj3YLZB8a_1qahoKBOkvWYWdKJ55cHJgI8TKnN4cB6IR4EQA2xeZUS7A1CDr3rJJm9H15txEf47hR9IJEax0o_NxzkKP_xW61kq8tn_ZBj1CcsNXtUlJIifxUvuRM9StWODtmO_SnV6Zts",
+    }
+)
+JWT_AUTH["JWT_PUBLIC_SIGNING_JWK_SET"] = json.dumps(
+    {
+        "keys": [
+            {
+                "kid": "openedx",
+                "kty": "RSA",
+                "e": "AQAB",
+                "n": "nP-wWVXa4Xh3VSV9SpEMJvV8dZPp_z2hUvuy97S11R-BL3FvhjSqlySPSXxedcwN9eNv18JcCF9-cub6DfBya2K_VYNTr0SCZDwKMoe74jVfCbHBXQtF4BHIbpR7zDExpfvkFYmqX3dhU5iYB6hDfS5ad4zkuDwBMWFFld8x-a9sY_ZNCRwe2-Fk6VfTn7o2DBi4owhlPoc03Z2cGW4j56PHXAJJtwJ41VAE2sQaf5rmv_9A_WHe0vtAr43kUwY2Xwn3IP3rJNacSPOtUm2t8z2UlAP7rwueeWo1NLpOlenNg1_J0BVylOnC4_lewef7JpsrbZgjtty8Rf7YNBebdw",
+            }
+        ]
+    }
+)
+JWT_AUTH["JWT_ISSUERS"] = [
+    {
+        "ISSUER": "http://local.overhang.io/oauth2",
+        "AUDIENCE": "openedx",
+        "SECRET_KEY": "nxZdmMLVeIqBLDPAuatdQaIr"
+    }
+]
+
+# Enable/Disable some features globally
+FEATURES["ENABLE_DISCUSSION_SERVICE"] = False
+FEATURES["PREVENT_CONCURRENT_LOGINS"] = False
+FEATURES["ENABLE_CORS_HEADERS"] = True
+
+# CORS
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ALLOW_INSECURE = True
+CORS_ALLOW_HEADERS = corsheaders_default_headers + ('use-jwt-cookie',)
+
+# Add your MFE and third-party app domains here
+CORS_ORIGIN_WHITELIST = []
+
+# Disable codejail support
+# explicitely configuring python is necessary to prevent unsafe calls
+import codejail.jail_code
+codejail.jail_code.configure("python", "nonexistingpythonbinary", user=None)
+# another configuration entry is required to override prod/dev settings
+CODE_JAIL = {
+    "python_bin": "nonexistingpythonbinary",
+    "user": None,
+}
+
+
+######## End of settings common to LMS and CMS
+
+######## Common CMS settings
+STUDIO_NAME = "Olive Test Dimitri - Studio"
+
+CACHES["staticfiles"] = {
+    "KEY_PREFIX": "staticfiles_cms",
+    "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    "LOCATION": "staticfiles_cms",
+}
+
+# Authentication
+SOCIAL_AUTH_EDX_OAUTH2_SECRET = "r9kEqwl9U9NoRSPNuWhQAPnm"
+SOCIAL_AUTH_EDX_OAUTH2_URL_ROOT = "http://lms:8000"
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = False  # scheme is correctly included in redirect_uri
+SESSION_COOKIE_NAME = "studio_session_id"
+
+MAX_ASSET_UPLOAD_FILE_SIZE_IN_MB = 100
+
+FRONTEND_LOGIN_URL = LMS_ROOT_URL + '/login'
+FRONTEND_REGISTER_URL = LMS_ROOT_URL + '/register'
+
+# Create folders if necessary
+for folder in [LOG_DIR, MEDIA_ROOT, STATIC_ROOT_BASE]:
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+
+
+######## End of common CMS settings
+
+ALLOWED_HOSTS = [
+    ENV_TOKENS.get("CMS_BASE"),
+    "cms",
+]
+CORS_ORIGIN_WHITELIST.append("http://studio.local.overhang.io")
+
+# Authentication
+SOCIAL_AUTH_EDX_OAUTH2_KEY = "cms-sso"
+SOCIAL_AUTH_EDX_OAUTH2_PUBLIC_URL_ROOT = "http://local.overhang.io"
+
+
+COURSE_AUTHORING_MICROFRONTEND_URL = "http://apps.local.overhang.io/course-authoring"
+
+
+LOGIN_REDIRECT_WHITELIST.append("apps.local.overhang.io")
+CORS_ORIGIN_WHITELIST.append("http://apps.local.overhang.io")
+CSRF_TRUSTED_ORIGINS.append("apps.local.overhang.io")
